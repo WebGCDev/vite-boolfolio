8cee37dc592642ceacd17a5fcd8368fffb4ce33c
WebGCDev <gaetano.chi@gmail.com>
2024-02-16 00:42:37 +0100

    Add: Bootstrap config in scss

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+// import default scss
+import './scss/style.scss';
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+
+createApp(App).mount('#app');
