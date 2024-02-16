72a44aee3b22d855408633505795afc9196f9407
WebGCDev <gaetano.chi@gmail.com>
2024-02-16 02:01:18 +0100

    Add: Axios + Milestone 3-4 Completed

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,40 @@
 <script>
+import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
+
 export default {
   name: 'Project Git',
+  data() {
+    return {
+      projects: [],
+      baseUrl: 'http://127.0.0.1:8000/',
+      apiUrls: {
+        projects: 'api/project',
+      },
+    };
+  },
   components: {
     AppHeader,
     AppMain,
     AppFooter,
+  },
+  methods: {
+    getProjects() {
+      axios
+        .get(this.baseUrl + this.apiUrls.projects)
+        .then((response) => {
+          console.log(response);
+          this.projects = response.data.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  created() {
+    this.getProjects();
   },
 };
 </script>
