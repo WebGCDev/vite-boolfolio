4e81617d8d55f309eb715967d8a578fdd9f3d5a1
WebGCDev <gaetano.chi@gmail.com>
2024-02-19 20:06:41 +0100

    Add: Shows project details and navigation between pages

--- src/pages/ProjectsDetails.vue
+++ src/pages/ProjectsDetails.vue
@@ -1,14 +1,48 @@
 <script>
+import axios from 'axios';
 export default {
   name: 'ProjectsDetails',
   created() {
     console.log(this.$route);
   },
+  data() {
+    return {
+      project: {},
+      baseUrl: 'http://127.0.0.1:8000/',
+      apiUrls: {
+        projects: 'api/project',
+      },
+    };
+  },
+  methods: {
+    getProjects() {
+      axios
+        .get(
+          this.baseUrl + this.apiUrls.projects + '/' + this.$route.params.slug
+        )
+        .then((response) => {
+          this.project = response.data.result;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  created() {
+    this.getProjects();
+  },
 };
 </script>
 <template>
   <div class="container my-4">
-    <h2>dettagli progetto</h2>
-    <li><router-link :to="{ name: 'home' }">torna alla home</router-link></li>
+    <h2>{{ project.title }}</h2>
+    <p>{{ project.description }}</p>
+    <li>
+      <router-link
+        class="text-decoration-none text-danger"
+        :to="{ name: 'projects' }"
+        >RITORNA AI PROGETTI</router-link
+      >
+    </li>
   </div>
 </template>
